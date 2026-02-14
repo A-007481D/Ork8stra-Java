37ae918c39483816c3b7c774c646f3bd55b30b62
A-007481D <labidabdelmalek@gmail.com>
2026-02-14 12:00:00 +0100

    refactor(core): migrate projects to inherit organization associations

--- src/main/java/com/ork8stra/api/dto/CreateProjectRequest.java
+++ src/main/java/com/ork8stra/api/dto/CreateProjectRequest.java
@@ -1,17 +1,16 @@
 package com.ork8stra.api.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * DTO: Create Project Request
  */
-
 
 @Data
 @Builder
@@ -19,12 +18,11 @@ import java.util.Map;
 @AllArgsConstructor
 public class CreateProjectRequest {
     private String name;
-    private String owner;
+    private UUID organizationId;
     private String gitUrl;
     private String branch;
     private String contextPath;
     private String dockerfilePath;
     private Map<String, String> env;
-
 
 }
